32228a69eed7bd55a542db057478908eab15e20e
Louie Jay Tutor <109414917+louiejaytutor@users.noreply.github.com>
2025-02-18 14:29:40 +0800

    Add files via upload

--- static/js/main.js
+++ static/js/main.js
@@ -64,22 +64,27 @@ const titleBar = document.getElementById("title-bar");
 let isDragging = false;
 let offsetX, offsetY;
 
-titleBar.addEventListener("mousedown", (e) => {
-  isDragging = true;
-  offsetX = e.clientX - windowElement.offsetLeft;
-  offsetY = e.clientY - windowElement.offsetTop;
-});
-
-document.addEventListener("mousemove", (e) => {
-  if (isDragging) {
-    windowElement.style.left = e.clientX - offsetX + "px";
-    windowElement.style.top = e.clientY - offsetY + "px";
-  }
-});
-
-document.addEventListener("mouseup", () => {
-  isDragging = false;
-});
+if (window.innerWidth > 500) {
+  titleBar.addEventListener("mousedown", (e) => {
+    isDragging = true;
+    offsetX = e.clientX - windowElement.offsetLeft;
+    offsetY = e.clientY - windowElement.offsetTop;
+  });
+  
+  document.addEventListener("mousemove", (e) => {
+    if (isDragging) {
+      windowElement.style.left = e.clientX - offsetX + "px";
+      windowElement.style.top = e.clientY - offsetY + "px";
+    }
+  });
+  
+  document.addEventListener("mouseup", () => {
+    isDragging = false;
+  });
+}
+else {
+  titleBar.style.cursor = "default";
+}
 
 backBtn.addEventListener("click", () => {
   sound.play();
